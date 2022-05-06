2b4d9ea084747e15ad225683bd24b87e89a6499b
asachiyigor <asachiyigor@gmail.com>
2022-05-06 12:08:55 +0300

    init

---
+++ src/test/java/ru/netology/OrderTest.java
@@ -0,0 +1,59 @@
+package ru.netology;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class OrderTest { //Версия 101.0.4951.54
+
+    private WebDriver driver;
+
+    @BeforeAll
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "D:/IT/chromedriver_win32/chromedriver.exe");
+    }
+
+    @BeforeEach
+    public void setUp2() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void close() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void test() {
+
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0)
+                .sendKeys("Асачий Игорь");
+        elements.get(1)
+                .sendKeys("+79119962069");
+        driver.findElement(By.className("checkbox__box"))
+              .click();
+        driver.findElement(By.className("button__content"))
+              .click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']"))
+                            .getText();
+        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+
+
+    }
+}
