3704d1314b461c28c9f6b6402ab638103233934a
asachiyigor <asachiyigor@gmail.com>
2022-05-12 16:29:38 +0300

    + 2 task update last

--- src/test/java/ru/netology/OrderTestNegative.java
+++ src/test/java/ru/netology/OrderTestNegative.java
@@ -4,13 +4,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class OrderTestNegative { //Версия 101.0.4951.54
